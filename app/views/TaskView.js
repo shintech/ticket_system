65c5451e71303e35b8e9b7282704a6da3afec9ad
Michael Prather <mprather1@gmail.com>
2016-12-07 04:04:17 +0000

    colored rows

--- app/views/TaskView.js
+++ app/views/TaskView.js
@@ -1,5 +1,16 @@
 var TaskView = Backbone.Marionette.View.extend({
   tagName: 'tr',
+  initialize: function(e){
+    // console.log(e.model.attributes)
+  },
+  id: function(e){
+    console.log(this.options.model.attributes.completed)
+    if (this.options.model.attributes.completed == true){
+      return 'completed'
+    } else {
+      return 'active'
+    }
+  },
   template: require("../templates/task-template.html")
 });
 
